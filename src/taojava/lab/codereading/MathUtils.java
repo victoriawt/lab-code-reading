60e1588923685f410e82ad1582fc3f19e0da04b8
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2013-09-03 09:08:02 -0500

    Add sample code files.

---
+++ src/taojava/lab/codereading/MathUtils.java
@@ -0,0 +1,23 @@
+package taojava.lab.codereading;
+
+/** 
+ * A simple collection of mathematical routines.
+ * 
+ * @author Samuel A. Rebelsky
+ */
+public class MathUtils {
+    /**
+     * Square an integer.
+     */
+    public static 
+    int square(int x) 
+    { return
+	    x * x;  } // square(int)
+    
+    /** 
+     * Square a double.
+     */
+    public static double square(double x) {
+	return x+x;
+    } // square(double)
+} // class MathUtils
